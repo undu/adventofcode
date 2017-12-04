7a9bf50987e697d463222559fa0917f055d1008a
Pau Ruiz i Safont <unduthegun@gmail.com>
2017-12-04 15:20:35 +0000

    second exercise!

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,10 +2,20 @@ const CAPTCHA: &'static str = include_str!("inverse_captcha.txt");
 
 fn main() {
     println!("Day 1; inverse captcha; First Star: {:?}", first_star());
+    println!("Day 1; inverse captcha; Second Star: {:?}", second_star());
 }
+
 fn first_star() -> u32 {
     CAPTCHA.chars()
         .zip(CAPTCHA.chars().cycle().skip(1))
+        .filter(|&(one, consec)| one.is_digit(10) && one == consec)
+        .map(|(one, _)| one.to_digit(10).expect("Impossibru"))
+        .sum()
+}
+
+fn second_star() -> u32 {
+    CAPTCHA.chars()
+        .zip(CAPTCHA.chars().cycle().skip(CAPTCHA.len() / 2))
         .filter(|&(one, consec)| one.is_digit(10) && one == consec)
         .map(|(one, _)| one.to_digit(10).expect("Impossibru"))
         .sum()
