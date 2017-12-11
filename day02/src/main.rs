35cefd7abac71aa6f5a0ec2ba65850204824d17f
Pau Ruiz i Safont <unduthegun@gmail.com>
2017-12-11 14:33:36 +0000

    Day 2!

---
+++ day02/src/main.rs
@@ -0,0 +1,31 @@
+const SPREADSHEET: &'static str = include_str!("spreadsheet.txt");
+
+fn main() {
+    println!("Day 2; corruption checksum; First Star: {:?}", first_star());
+    println!("Day 2; corruption checksum; Second Star: {:?}", second_star());
+}
+
+fn first_star() -> u32 {
+    SPREADSHEET.lines()
+        .map(|row| row.split_whitespace()
+             .map(|num| num.parse().unwrap()).collect::<Vec<u32>>())
+        .map(|row| row.iter().max().unwrap() - row.iter().min().unwrap())
+        .sum()
+}
+
+fn second_star() -> u32 {
+    SPREADSHEET.lines()
+        .map(|row| row.split_whitespace()
+             .map(|num| num.parse().unwrap()).collect::<Vec<u32>>())
+        .map(|row| {
+            for n in &row {
+                for m in &row {
+                    if n % m == 0 && n != m {
+                        return n / m;
+                    }
+                }
+            }
+            0
+        })
+        .sum()
+}
