d91d315aaecde0a898245717609a30010ca6d138
Pau Ruiz i Safont <unduthegun@gmail.com>
2018-01-10 16:13:19 +0000

    Add example day

---
+++ 00/src/main.rs
@@ -0,0 +1,13 @@
+const MAIN: &'static str = include_str!("main.rs");
+
+fn main() {
+    println!("Day 00; Title; First Star: {:?}", first_star());
+    println!("Day 00; Title; Second Star: {:?}", second_star());
+}
+
+fn first_star() -> u32 {
+    0
+}
+fn second_star() -> u32 {
+    0
+}
